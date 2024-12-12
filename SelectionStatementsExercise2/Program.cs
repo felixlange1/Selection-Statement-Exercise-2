8c17a61c70e7bf4e894f28aed1eba31a465b6aaa
Felix Lange <felixlange1@gmail.com>
2024-12-11 23:08:18 -0800

    switch statement added

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is awesome!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is cool because it's all about the real world.");
+                    break;
+                case "history":
+                    Console.WriteLine("A lot of people underestimate the value of history.");
+                    break;
+                case "English":
+                    Console.WriteLine("English is a great choice.");
+                    break;
+                default:
+                    Console.WriteLine("That's a cool subject.");
+                    break;
+            }
         }
     }
 }
